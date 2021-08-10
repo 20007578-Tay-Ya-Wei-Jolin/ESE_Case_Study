5938adaa7ad56da3fc3918c7164c87715000241b
19010398-Ng-TengYik <68002365+19010398-Ng-TengYik@users.noreply.github.com>
2021-08-10 18:16:28 +0800

    Update Appointment.java

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -2,13 +2,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-/**
- * I declare that this code was written by me. I will not copy or allow others
- * to copy my code. I understand that copying code is considered as plagiarism.
- *
- * 19010398, 9 Aug 2021 4:12:16 pm
- */
-
 public class Appointment {
 
 	private String date;
