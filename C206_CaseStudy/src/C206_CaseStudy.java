568202dedc738398b2b99e67a0fc964fe02b7427
19010398 <19010398@DESKTOP-G1Q8REC>
2021-08-10 10:42:15 +0800

    Modified CaseStudy.java and added in Helper.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,8 +2,65 @@
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int option = 0;
+
+		while (option != 5) {
+
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				// Visitor Registration
+
+			} else if (option == 2) {
+				// View cars
+
+			} else if (option == 3) {
+				// View parts
+
+			} else if (option == 4) {
+				// Display appointment
+
+			} else if (option == 5) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
+	}
+	
+	public static void menu() {
+
+		System.out.println("1. Visitor Registration");
+		System.out.println("2. View cars");
+		System.out.println("3. View parts");
+		System.out.println("4. Display appointment schedule");
+		System.out.println("5. Quit");
+		Helper.line(80, "-");
 
 	}
+	
+	//================================= Option 1 View Registration =================================
+	//Write code for View Registration option
+	
+	
+	
+	
+	//================================= Option 2 View cars =================================
+	//Write code for View cars option
+	
+	
+	
+	
+	//================================= Option 3 View parts =================================
+	//Write code for View parts option
+	
+	
+	
+	
+	//================================= Option 4 Display appointment schedule =================================
+	//Write code for Display appointment schedule option
+	
 
 }
